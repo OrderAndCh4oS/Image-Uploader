ad495d9c13afc41caa432b436d13f159eafe05d6
Sarcoma <sarcoma@live.co.uk>
2018-01-29 10:21:59 +0000

    Add Profiler

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
